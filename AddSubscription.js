9916397b647ba608b94e624478e019dca220144a
Justin Rodriguez <rodriguezjustin657@gmail.com>
2020-06-18 19:50:35 -0500

    add subscription

--- AddSubscription.js
+++ AddSubscription.js
@@ -1,2 +1,47 @@
-var propId = document.location.search.replace(/^.*?\=/,'');
-console.log("propId = " + propId);
+(function () {
+    console.log("dashboard")
+
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log("Logged in as " + firebaseUser.uid);
+            var propId = document.location.search.replace(/^.*?\=/, '');
+            console.log("propId = " + propId);
+            fetchPropDetails(firebaseUser.uid, propId);
+            attachFunctionalityToInputs(firebaseUser.uid, propId);
+        } else {
+            console.log('not logged in');
+            window.location = "index.html";
+        }
+    })
+
+}());
+
+function fetchPropDetails(uid, propId) {
+    const addressTitle = document.getElementById('addressTitle');
+    firebase.database().ref('/users/' + uid + '/properties/' + propId).once('value').then(function(snapshot) {
+        console.log(snapshot.val());
+        addressTitle.innerHTML = snapshot.val().address;
+    });
+}
+
+function attachFunctionalityToInputs(uid, propId) {
+    console.log("attach: " + uid + " " + propId);
+    const txtDimensions = document.getElementById('txtDimensions');
+    const txtFrequency = document.getElementById('txtFrequency');
+    const txtStartDate = document.getElementById('txtStartDate');
+    const btnCreateSub = document.getElementById('btnCreateSub');
+
+    btnCreateSub.addEventListener('click', e => {
+        console.log(txtDimensions.value + " " + txtFrequency.value + " " + txtStartDate.value);
+        firebase.database().ref('users/' + uid + '/properties/' + propId + '/subscription').set({
+            dimensions : txtDimensions.value,
+            frequency : txtFrequency.value,
+            nextDeliveryDate: txtStartDate.value,
+        }).then(e => {
+            console.log("subscription added!");
+            console.log(e);
+            window.location = "dashboard.html";
+        })
+    })
+}
--- app.js
+++ app.js
@@ -1,55 +1,9 @@
 (function () {
 
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-  const btnLogout = document.getElementById('btnLogout');
-
-  btnLogin.addEventListener('click', e => {
-
-    console.log("Login button pressed");
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    //Sign in
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-
-  })
-
-  btnSignUp.addEventListener('click', e => {
-
-    console.log("Sign Up button pressed");
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    firebase.auth().createUserWithEmailAndPassword(email, pass).then(e => {
-      console.log("create user")
-      console.log(e.uid);
-      firebase.database().ref('users/' + e.uid).set({
-        uid: e.uid,
-        email: email
-      }).then(e => {
-        console.log("user data set");
-        console.log(e);
-        window.location = "dashboard.html";
-      })
-    }).catch(function (error) {
-      // Handle Errors here.
-      console.log("error in creating user " + error)
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
-
-  })
-
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
       console.log("Logged in as " + firebaseUser.uid);
+      window.location = "dashboard.html";
     } else {
       console.log('not logged in');
     }
